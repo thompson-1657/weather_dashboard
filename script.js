720fc941570d31530fe0a8fcaa2afb5b0ad9928b
Rachel Thompson <thompson.1657@gmail.com>
2020-12-13 17:09:05 -0600

    added uv and uv colors

--- script.js
+++ script.js
@@ -22,7 +22,54 @@ $(".btn-primary").on("click", function () {
         var searchHistory = $("#entered-city").val()
         localStorage.setItem("city", searchHistory)
 
+
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        console.log(lat);
+        console.log(lon);
+
+        var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=a1d5f0111e093d1e307cd296b74d63f2"
+
+        $.ajax({
+            url: queryURL2,
+            method: "GET",
+        }).then(function (uvResponse) {
+            var uvIndex = uvResponse.value;
+            console.log(typeof uvIndex);
+            $(".current-uv").text("UV Index: " + uvIndex);
+
+            if (uvIndex < 3) {
+                $(".current-uv").css("color", "green")
+            } else if (uvIndex >= 3 && uvIndex <= 5.99) {
+                $(".current-uv").css("color", "orange")
+            } else {
+                $(".current-uv").css("color", "red")
+            }
+        });
+
+
+
+
+
+
+
+
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
