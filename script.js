3a42846bf60186fd41e0a71ceeac36d84a767fa9
Rachel Thompson <thompson.1657@gmail.com>
2020-12-15 17:43:29 -0600

    current date icon added

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(".btn-primary").on("click", function () {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        //console.log(response)
+        console.log(response)
         $(".current-city").html(response.name + " " + time)
         $(".current-humidity").text("Humidity: " + response.main.humidity + " %")
         $(".current-wind").text("Wind Speed: " + response.wind.speed + " MPH")
@@ -21,9 +21,15 @@ $(".btn-primary").on("click", function () {
 
         var searchHistory = $("#entered-city").val();
         localStorage.setItem("city", searchHistory);
-        //console.log(searchHistory);
 
 
+        var weatherIcon = $("<img>");
+        weatherIcon.attr(
+            "src",
+            "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
+        );
+        $("#wicon0").empty();
+        $("#wicon0").append(weatherIcon);
 
 
 
@@ -51,7 +57,7 @@ $(".btn-primary").on("click", function () {
                 $(".current-uv").css("color", "red")
             }
 
-            var cnt = 6
+            var cnt = 50
             var queryURLFiveDay = "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&cnt=" + cnt + "&appid=a1d5f0111e093d1e307cd296b74d63f2&q=" +
                 citySearch
 
@@ -65,25 +71,30 @@ $(".btn-primary").on("click", function () {
 
 
                 var fiveDay = fiveDayResponse.list
-                //console.log(fiveDay);
-                for (var i = 0; i < fiveDay.length; i++) {
+                console.log(fiveDay);
+                for (var i = 0; i < fiveDay.length; i += 8) {
                     //console.log(fiveDay[i]);
 
 
                     $(".for-temp1").text("Temp: " + fiveDay[1].main.temp + " °F")
                     $(".for-humid1").text("Humidity: " + fiveDay[1].main.humidity + " %")
+                    //var currentIcon1 = (response.weather[1].icon)
 
-                    $(".for-temp2").text("Temp: " + fiveDay[2].main.temp + " °F")
-                    $(".for-humid2").text("Humidity: " + fiveDay[2].main.humidity + " %")
+                    $(".for-temp2").text("Temp: " + fiveDay[8].main.temp + " °F")
+                    $(".for-humid2").text("Humidity: " + fiveDay[8].main.humidity + " %")
+                    //var currentIcon2 = (response.weather[2].icon)
 
-                    $(".for-temp3").text("Temp: " + fiveDay[3].main.temp + " °F")
-                    $(".for-humid3").text("Humidity: " + fiveDay[3].main.humidity + " %")
+                    $(".for-temp3").text("Temp: " + fiveDay[16].main.temp + " °F")
+                    $(".for-humid3").text("Humidity: " + fiveDay[16].main.humidity + " %")
+                    //var currentIcon3 = (response.weather[3].icon)
 
-                    $(".for-temp4").text("Temp: " + fiveDay[4].main.temp + " °F")
-                    $(".for-humid4").text("Humidity: " + fiveDay[4].main.humidity + " %")
+                    $(".for-temp4").text("Temp: " + fiveDay[24].main.temp + " °F")
+                    $(".for-humid4").text("Humidity: " + fiveDay[24].main.humidity + " %")
+                    //var currentIcon4 = (response.weather[4].icon)
 
-                    $(".for-temp5").text("Temp: " + fiveDay[5].main.temp + " °F")
-                    $(".for-humid5").text("Humidity: " + fiveDay[5].main.humidity + " %")
+                    $(".for-temp5").text("Temp: " + fiveDay[32].main.temp + " °F")
+                    $(".for-humid5").text("Humidity: " + fiveDay[32].main.humidity + " %")
+                    //var currentIcon5 = (response.weather[5].icon)
                 }
             })
 
@@ -96,16 +107,16 @@ $(".btn-primary").on("click", function () {
     var time2 = moment().add(1, 'days').format('L');
     $(".current-date2").html(time2)
 
-    var time3 = moment().add(1, 'days').format('L');
+    var time3 = moment().add(2, 'days').format('L');
     $(".current-date3").html(time3)
 
-    var time4 = moment().add(1, 'days').format('L');
+    var time4 = moment().add(3, 'days').format('L');
     $(".current-date4").html(time4)
 
-    var time5 = moment().add(1, 'days').format('L');
+    var time5 = moment().add(4, 'days').format('L');
     $(".current-date5").html(time5)
 
-    var time6 = moment().add(1, 'days').format('L');
+    var time6 = moment().add(5, 'days').format('L');
     $(".current-date6").html(time6)
 
 
