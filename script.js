6a5445b8e831f29c6debd93165abce5a02a3ed99
Rachel Thompson <thompson.1657@gmail.com>
2020-12-16 21:13:41 -0600

    icons working all days

--- script.js
+++ script.js
@@ -36,8 +36,7 @@ $(".btn-primary").on("click", function () {
 
         var lat = response.coord.lat;
         var lon = response.coord.lon;
-        //console.log(lat);
-        //console.log(lon);
+
 
         var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=a1d5f0111e093d1e307cd296b74d63f2"
 
@@ -66,36 +65,85 @@ $(".btn-primary").on("click", function () {
                 url: queryURLFiveDay,
                 method: "GET",
             }).then(function (fiveDayResponse) {
-                //console.log(fiveDayResponse);
+                console.log(fiveDayResponse);
+
+
+
 
 
 
                 var fiveDay = fiveDayResponse.list
                 console.log(fiveDay);
-                for (var i = 0; i < fiveDay.length; i += 8) {
-                    //console.log(fiveDay[i]);
 
 
-                    $(".for-temp1").text("Temp: " + fiveDay[1].main.temp + " °F")
-                    $(".for-humid1").text("Humidity: " + fiveDay[1].main.humidity + " %")
-                    //var currentIcon1 = (response.weather[1].icon)
 
-                    $(".for-temp2").text("Temp: " + fiveDay[8].main.temp + " °F")
-                    $(".for-humid2").text("Humidity: " + fiveDay[8].main.humidity + " %")
-                    //var currentIcon2 = (response.weather[2].icon)
+                var weatherIcon2 = $("<img>");
+                weatherIcon2.attr(
+                    "src",
+                    "https://openweathermap.org/img/w/" + fiveDay[1].weather[0].icon + ".png"
+                );
+                $("#wicon2").empty();
+                $("#wicon2").append(weatherIcon2)
 
-                    $(".for-temp3").text("Temp: " + fiveDay[16].main.temp + " °F")
-                    $(".for-humid3").text("Humidity: " + fiveDay[16].main.humidity + " %")
-                    //var currentIcon3 = (response.weather[3].icon)
+                var weatherIcon3 = $("<img>");
+                weatherIcon3.attr(
+                    "src",
+                    "https://openweathermap.org/img/w/" + fiveDay[8].weather[0].icon + ".png"
+                );
+                $("#wicon3").empty();
+                $("#wicon3").append(weatherIcon3)
 
-                    $(".for-temp4").text("Temp: " + fiveDay[24].main.temp + " °F")
-                    $(".for-humid4").text("Humidity: " + fiveDay[24].main.humidity + " %")
-                    //var currentIcon4 = (response.weather[4].icon)
+                var weatherIcon4 = $("<img>");
+                weatherIcon4.attr(
+                    "src",
+                    "https://openweathermap.org/img/w/" + fiveDay[16].weather[0].icon + ".png"
+                );
+                $("#wicon4").empty();
+                $("#wicon4").append(weatherIcon4)
 
-                    $(".for-temp5").text("Temp: " + fiveDay[32].main.temp + " °F")
-                    $(".for-humid5").text("Humidity: " + fiveDay[32].main.humidity + " %")
-                    //var currentIcon5 = (response.weather[5].icon)
-                }
+                var weatherIcon5 = $("<img>");
+                weatherIcon5.attr(
+                    "src",
+                    "https://openweathermap.org/img/w/" + fiveDay[24].weather[0].icon + ".png"
+                );
+                $("#wicon5").empty();
+                $("#wicon5").append(weatherIcon5)
+
+                var weatherIcon6 = $("<img>");
+                weatherIcon6.attr(
+                    "src",
+                    "https://openweathermap.org/img/w/" + fiveDay[32].weather[0].icon + ".png"
+                );
+                $("#wicon6").empty();
+                $("#wicon6").append(weatherIcon6)
+
+
+
+
+                $(".for-temp1").text("Temp: " + fiveDay[1].main.temp + " °F")
+                $(".for-humid1").text("Humidity: " + fiveDay[1].main.humidity + " %")
+
+
+                $(".for-temp2").text("Temp: " + fiveDay[8].main.temp + " °F")
+                $(".for-humid2").text("Humidity: " + fiveDay[8].main.humidity + " %")
+
+
+                $(".for-temp3").text("Temp: " + fiveDay[16].main.temp + " °F")
+                $(".for-humid3").text("Humidity: " + fiveDay[16].main.humidity + " %")
+
+
+                $(".for-temp4").text("Temp: " + fiveDay[24].main.temp + " °F")
+                $(".for-humid4").text("Humidity: " + fiveDay[24].main.humidity + " %")
+
+
+                $(".for-temp5").text("Temp: " + fiveDay[32].main.temp + " °F")
+                $(".for-humid5").text("Humidity: " + fiveDay[32].main.humidity + " %")
+
+
+
+
+
+
             })
 
 
@@ -118,6 +166,7 @@ $(".btn-primary").on("click", function () {
 
     var time6 = moment().add(5, 'days').format('L');
     $(".current-date6").html(time6)
+
 
 
 
